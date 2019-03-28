984aee32d42e84502a483845d6fa6314c5f907da
Evgeniy.Skiba <eugeniy.skiba@gmail.com>
2019-03-28 12:08:36 +0200

    initial commit on notebook

---
+++ Core/ObjectRelations/MetanitSample/MetanitSample/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetanitSample
+{
+    public class Car
+    {
+        ElectricEngine engine;
+        public Car()
+        {
+            engine = new ElectricEngine();
+        }
+    }
+}
